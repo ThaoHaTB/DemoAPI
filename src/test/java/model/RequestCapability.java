4ca9dc3b8a413337b79c33bc457935f53b03acb4
thaoha93 <44538603+ThaoHaTB@users.noreply.github.com>
2023-04-22 23:01:22 +0700

    Update RequestCapability.java

--- src/test/java/model/RequestCapability.java
+++ src/test/java/model/RequestCapability.java
@@ -6,7 +6,7 @@ import java.util.function.Function;
 
 public interface RequestCapability {
     public static final String EMAIL = "hathaok37cntt@gmail.com";
-    public static final String API_TOKEN = "ATATT3xFfGF0eoyhMAHQWvLB4_osNbWokbSZW0WB-1DbQaCpjfS_a4yLILK2I5XOD6v-DspTeQlB3Oms92neohtB8ErQVD5mFl4iLL5kcOiPyFTBPZWOwCwwPbk3hzq9D4KH_zmTUl43BKcO1Cae73Ni-8XjScJFsZHY6wXrgRdVhoMlTG2ztbw=88DE4F99";
+    public static final String API_TOKEN = "ATATT3xFfGF0t3OR9UeKZEjSwC5UPxEiIMQpV3Yv9nKT_xTe403tIHvdwFjoxO0L7zQcT8cYqIEg0yB1YKEJqR3GCaeefyvJQG1WvWCm5eLdZDBuCp4Vlw5vKcSrQZ7OeZa59AsbUElOWHQlSRPPIogtBbR8eJX68DcZQQjtmD2ExKvWtzQLFuM=F11CE1CC";
     Header defaultHeader = new Header("Content-type", "application/json;charset=UTF-8");
     Header acceptJsonHeader=new Header("Accept","application/json");
 
