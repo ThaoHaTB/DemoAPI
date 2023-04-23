3c737b2a02e74abe5e574c3ae4b0e3e231eac268
thaoha93 <44538603+ThaoHaTB@users.noreply.github.com>
2023-04-23 11:48:18 +0700

    Update RequestCapability.java

--- src/test/java/model/RequestCapability.java
+++ src/test/java/model/RequestCapability.java
@@ -6,7 +6,7 @@ import java.util.function.Function;
 
 public interface RequestCapability {
     public static final String EMAIL = "hathaok37cntt@gmail.com";
-    public static final String API_TOKEN = "ATATT3xFfGF0y9UB9YnOkIHxZec-_x8mmou-MYQ7UVYB06Gnep1XVVRaBwOqGOLexTjse6c1l9X7VsSu7wA40Q9yiOfbCufkyUzYfqSvguUkpFNbeZF9J7qSloYanUSd1tlHDKmyIVeUTeHxrUs13g8P9meQ3fsfg6qlsD5TaX1IAvXQoDQQ7C4=72708EDF";
+    public static final String API_TOKEN = "";
     Header defaultHeader = new Header("Content-type", "application/json;charset=UTF-8");
     Header acceptJsonHeader=new Header("Accept","application/json");
 
